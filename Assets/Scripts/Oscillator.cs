d9b76f71640ab7cbc6268c720fd16826abc1d9ad
Aria Akbariyeh <aria.ak@hotmail.com>
2024-08-17 18:09:48 -0400

    Protect Period in Oscillator from NaN errors

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -19,6 +19,7 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period == Mathf.Epsilon) {return; }
         float cycles = Time.time / period;
         float tau = 2 * Mathf.PI;
 
